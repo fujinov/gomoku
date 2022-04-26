22a2cb8347e164466092daa0c6ed0ed9f2178bbb
fujinov <98008144+fujinov@users.noreply.github.com>
2022-04-26 11:51:53 +0900

    石の斜めの判定を追加

--- gomoku.py
+++ gomoku.py
@@ -61,7 +61,62 @@ class Gobang():
             if self._count == 5:
                 stone_coordinates = [(yj, x) for yj in range(yi, yi-5, -1)]
                 return stone_coordinates
+
+        # 斜め（\方向）の判定
+        if y > x:
+            start_y = y - x
+            start_x = 0
+            end_y = 15
+            end_x = x + 15 - y
+        elif x > y:
+            start_y = 0
+            start_x = x - y
+            end_y = y + 15 - x
+            end_x = 15
+        else:
+            start_y, start_x = 0, 0
+            end_y, end_x = 15, 15
+
+        self._count = 1
+        for yi, xi in zip(range(start_y, end_y), range(start_x, end_x)):
+            if yi == 0 or xi == 0:
+                before_stone = 0
+            else:
+                before_stone = self._board[yi-1][xi-1]
+
+            self._count_color_stone(yi, xi, before_stone)
+
+            if self._count == 5:
+                stone_coordinates = [(yj, xj) for yj, xj in zip(range(yi, yi-5, -1), range(xi, xi-5, -1))]
+                return stone_coordinates
+
+        # 斜め（/方向）の判定
+        if y + x <= 14:
+            start_y = 0
+            start_x = y + x
+            end_y = y + x + 1
+            end_x = -1
+        else:
+            start_y = y + x -14
+            start_x = 14
+            end_y =  15
+            end_x = start_y - 1
+
+        self._count =1
+        for yi, xi in zip(range(start_y, end_y), range(start_x, end_x, -1)):
+            if yi == start_y and xi == start_x:
+                before_stone = 0
+            else:
+                before_stone = self._board[yi-1][xi+1]
+
+            self._count_color_stone(yi, xi, before_stone)
             
+            if self._count == 5:
+                stone_coordinates = [(yj, xj) for yj, xj in zip(range(yi, yi-5, -1), range(xi, xi+5))]
+                return stone_coordinates
+        return []
+
+
 def create_layout():
     layout = [[sg.Push(), sg.Text('白のターン', font=(None, 10), key='-TURN-')]]
     for y in range(15):
